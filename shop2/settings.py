8240d0c764d0a28678d85741395ca1e72324c2cf
NikGlad <90272508+NikGlad@users.noreply.github.com>
2022-12-27 08:38:15 +0300

    27.12

--- shop2/settings.py
+++ shop2/settings.py
@@ -15,9 +15,9 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Quick-start development settings - unsuitable for production
